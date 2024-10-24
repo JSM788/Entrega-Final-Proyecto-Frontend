9d1860dc07fbe2fe1b259e7adab68576aaac5f4a
Administrator <raulgm00@gmail.com>
2024-10-23 21:00:04 -0600

    Estructura base Header/Navbvar/Footer
    npm i @material-tailwind/react
    npm install react-router-dom

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import style from '../Components/Styles/Footer.module.css'
+import styles from '../Components/Styles/Footer.module.css'
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
