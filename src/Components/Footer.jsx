136682f3ec1bd11a4723ff59a9c0f40a0bfcb84e
iBustamantec3v <ivanebustamantec3v@hotmail.com>
2024-10-23 19:24:13 -0700

    estrecturando footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import style from '../Components/Styles/Footer.module.css'
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+      <>
+        <div className={styles.bar}>
+          <Link to='https://es-la.facebook.com/'>
+            <img
+              src='../images/ico-facebook.png'
+              alt='iconFacebook'
+              className={styles.socialneticon}
+            /></Link>
+          <Link to='https://www.instagram.com/'>
+            <img
+              src='../images/ico-instagram.png'
+              alt='iconInsta'
+              className={styles.socialneticon}
+            /></Link>
+          <Link to='https://web.whatsapp.com/'>
+            <img
+              src='../images/ico-whatsapp.png'
+              alt='iconWhatsapp'
+              className={styles.socialneticon}
+            /></Link>
+        </div>
+      </>
+    );
+  };
+  
+  export default Footer
+  
