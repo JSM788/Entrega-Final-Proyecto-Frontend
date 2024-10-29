ead4259adcf714c93974ceb8bebc5a7c66234294
Ivan Bustamante <ivanebustamantec3v@hotmail.com>
2024-10-29 01:32:47 +0000

    Configurando footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,30 +4,40 @@ import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-      <>
+      <footer className={styles.footer}>
         <div className={styles.bar}>
-          <Link to='https://es-la.facebook.com/'>
-            <img
-              src='../images/ico-facebook.png'
-              alt='iconFacebook'
-              className={styles.socialneticon}
-            /></Link>
-          <Link to='https://www.instagram.com/'>
-            <img
-              src='../images/ico-instagram.png'
-              alt='iconInsta'
-              className={styles.socialneticon}
-            /></Link>
-          <Link to='https://web.whatsapp.com/'>
-            <img
-              src='../images/ico-whatsapp.png'
-              alt='iconWhatsapp'
-              className={styles.socialneticon}
-            /></Link>
+          <div className={styles.logoAndText}>
+            <h1>
+              MOVEIT
+            </h1>
+            <div className={styles.footerText}>
+              <h2>@2024 Digital Booking</h2>
+              <h3>Proyecto Integrador - Grupo 4</h3>
+            </div>
+          </div>
+          <div className={styles.socialLinks}>
+            <Link to='https://www.instagram.com/'>
+              <img
+                src='../images/ico-instagram.png'
+                alt='iconInsta'
+                className={styles.socialneticon}
+              /></Link>
+            <Link to='https://es-la.facebook.com/'>
+              <img
+                src='../images/ico-facebook.png'
+                alt='iconFacebook'
+                className={styles.socialneticon}
+              /></Link>
+            <Link to='https://x.com/'>
+              <img
+                src='../images/ico-x.png'
+                alt='iconX'
+                className={styles.socialneticon}
+              /></Link>
+          </div>
         </div>
-      </>
+      </footer>
     );
   };
   
   export default Footer
-  
