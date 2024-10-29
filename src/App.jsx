aca2774a3866fa9e4aeaa89db4c3ae56ae700930
Diego Juárez <diegojve27@gmail.com>
2024-10-29 01:50:35 +0000

    Feature 002

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import './App.css'
-import Footer from "./Components/Footer.jsx"
-import NavbarComponent from "./Components/NavbarComponent.jsx"
-import { NavbarSimple } from './Components/NavbarSimple.jsx'
-import StickyNavbar from './Components/StickyNavbar.jsx'
+import "./App.css";
+import Footer from "./Components/Footer.jsx";
+import { HomeContainer } from "./Components/HomeContainer.jsx";
+import NavbarComponent from "./Components/NavbarComponent.jsx";
+import { NavbarSimple } from "./Components/NavbarSimple.jsx";
+import StickyNavbar from "./Components/StickyNavbar.jsx";
 
 function App() {
-  
   return (
-    <>
-      <StickyNavbar/>
-      <Footer/>
-    </>
-    
-
-
-  )
+    <div className="flex flex-col min-h-screen">
+      <StickyNavbar />
+      <main className="flex-grow bg-white">
+        <HomeContainer />
+      </main>
+      {/*<Footer /> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/Components/StickyNavbar.jsx
+++ src/Components/StickyNavbar.jsx
@@ -7,17 +7,17 @@ import {
   IconButton,
   Card,
 } from "@material-tailwind/react";
- 
+
 export function StickyNavbar() {
   const [openNav, setOpenNav] = React.useState(false);
- 
+
   React.useEffect(() => {
     window.addEventListener(
       "resize",
-      () => window.innerWidth >= 960 && setOpenNav(false),
+      () => window.innerWidth >= 960 && setOpenNav(false)
     );
   }, []);
- 
+
   const navList = (
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
@@ -52,7 +52,7 @@ export function StickyNavbar() {
       </Typography>
     </ul>
   );
- 
+
   return (
     <>
       <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
@@ -133,35 +133,8 @@ export function StickyNavbar() {
           </div>
         </MobileNav>
       </Navbar>
-      <div className="mx-auto max-w-screen-md py-12">
-        <Card className="mb-12 overflow-hidden">
-          <img
-            alt="nature"
-            className="h-[32rem] w-full object-cover object-center"
-            src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2717&q=80"
-          />
-        </Card>
-        <Typography variant="h2" color="blue-gray" className="mb-2">
-          What is Material Tailwind
-        </Typography>
-        <Typography color="gray" className="font-normal">
-          Can you help me out? you will get a lot of free exposure doing this
-          can my website be in english?. There is too much white space do less
-          with more, so that will be a conversation piece can you rework to make
-          the pizza look more delicious other agencies charge much lesser can
-          you make the blue bluer?. I think we need to start from scratch can my
-          website be in english?, yet make it sexy i&apos;ll pay you in a week
-          we don&apos;t need to pay upfront i hope you understand can you make
-          it stand out more?. Make the font bigger can you help me out? you will
-          get a lot of free exposure doing this that&apos;s going to be a chunk
-          of change other agencies charge much lesser. Are you busy this
-          weekend? I have a new project with a tight deadline that&apos;s going
-          to be a chunk of change. There are more projects lined up charge extra
-          the next time.
-        </Typography>
-      </div>
     </>
   );
 }
 
-export default StickyNavbar
+export default StickyNavbar;
