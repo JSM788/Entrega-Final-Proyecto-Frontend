021ea1b9bfbafb2e8ac57109edf824e9b92977ce
Diego Juárez <diegojve27@gmail.com>
2024-11-10 18:18:04 +0000

    Feature 012

---
+++ src/Components/Page403.jsx
@@ -0,0 +1,10 @@
+const Page403 = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen text-center">
+      <h2 className="text-4xl font-bold text-red-600 mb-4">Forbidden: 403</h2>
+      <p className="text-lg text-gray-700">No tienes permisos para acceder a esta página.</p>
+    </div>
+  )
+}
+
+export default Page403;
--- src/Components/utils/reducer.js
+++ src/Components/utils/reducer.js
@@ -1,6 +1,21 @@
+const loginLs = localStorage.getItem('login') ? JSON.parse(localStorage.getItem('login')) : false;
+const userLs = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null;
+const tokenLs = localStorage.getItem('accessToken') || "";
+
+export const initialState = {
+    vehicles: [],
+    isAuth: loginLs,
+    user: userLs,
+    accessToken: tokenLs
+  };
+
 export const reducer = (state, action) => {
     switch (action.type) {
         case "SET_VEHICLES":
             return{...state, vehicles: action.payload}
+        case "login": 
+            return { ...state, isAuth: true, user: action.payload, accessToken: action.payload.accessToken }
+        case "logout":
+            return { ...state, isAuth: false, user:[], accessToken: "" }
     }
 }
